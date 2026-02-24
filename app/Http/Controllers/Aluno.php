300e799fbcc0a1251b955dfb27be00f91409ad94
Rick-delux3 <henriquerodriguesneves4@gmail.com>
2026-02-24 14:53:54 -0300

    Aula_24/02

---
+++ app/Http/Controllers/Aluno.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Consultas extends Controller
+{
+    public function Vida academica(){
+        return "login";
+    }
+    public function Login(){
+        return "login";
+    }
+    public function Login(){
+        return "login";
+    }
+    public function Login(){
+        return "login";
+    }
+    public function Login(){
+        return "login";
+    }
+
+    
+}
---
+++ app/Http/Controllers/Contato.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Contato extends Controller
+{
+    function contato(){
+        echo 'Página contato';
+    }
+
+    public function 
+
+    
+}
+
+
